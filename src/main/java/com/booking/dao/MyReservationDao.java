2d13cef963b1fadcf2a56e47e42c6e08186c7820
smilesj <19smilesj@gmail.com>
2018-01-15 01:27:46 +0900

    나의 예약

---
+++ src/main/java/com/booking/dao/MyReservationDao.java
@@ -0,0 +1,12 @@
+package com.booking.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.booking.dto.MyReservationDto;
+
+@Repository
+public interface MyReservationDao {
+	public List<MyReservationDto> selectAll();
+}
---
+++ src/main/java/com/booking/service/MyReservationService.java
@@ -0,0 +1,9 @@
+package com.booking.service;
+
+import java.util.List;
+
+import com.booking.dto.MyReservationDto;
+
+public interface MyReservationService {
+	public List<MyReservationDto> selectAll();
+}

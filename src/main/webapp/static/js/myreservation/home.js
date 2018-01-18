970024f5cd88c419dd66003616c2c9cf1e1d5b37
smilesj <19smilesj@gmail.com>
2018-01-18 18:52:17 +0900

    [FE] my예약

--- src/main/webapp/static/js/myreservation/home.js
+++ src/main/webapp/static/js/myreservation/home.js
@@ -5,17 +5,18 @@ $(function () {
 $(".summary_board li").on("click", function(){
 	$(this).find("a").addClass("on");
 });
-
+var reservationId;
 $(".booking_cancel").on("click", function(){
-	var reservationId = $(this).attr("data-reservation-id");
+	reservationId = $(this).attr("data-reservation-id");
 	$(".popup_booking_wrapper").css("display", "block");
+	$(".popup_booking_wrapper").attr("data-reservation-id", reservationId);
 });
 
 
 $(".popup_btn_close").on("click", closePopup);
 $(".btn_gray").on("click", closePopup);
 $(".popup_booking_wrapper").click(".btn_green", function(){
-	var reservationId = $(this).attr("data-reservation-id");
+	//reservationId = $(this).attr("data-reservation-id");
 	console.log(reservationId);
 //	$.ajax({
 //		url : "/api/reservation/",

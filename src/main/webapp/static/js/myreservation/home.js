161380b1c7e8ee8ee20884017cd29222a79cfaa9
smilesj <19smilesj@gmail.com>
2018-01-22 13:52:12 +0900

    리팩토링

--- src/main/webapp/static/js/myreservation/home.js
+++ src/main/webapp/static/js/myreservation/home.js
@@ -1,55 +1,109 @@
 $(function () {
-    console.log('hello');
+    console.log('hello my reservation');
+    MyReservation.init();
+    Popup.init();
 });
 
-var summaryIndex = 0;
-$(".my_summary li").on("click", function(){
-	$(this).parent().find("li").eq(summaryIndex).find("a").removeClass("on");
-	summaryIndex = $(this).index();
-	$(this).find("a").addClass("on");
+var Popup = (function(){
+	var popup_wrapper = $(".popup_booking_wrapper");
+	var reservationId;
+	var title;
+	var time;
 	
-	var list_cards = $(".list_cards li");
-	if(summaryIndex == 0){
-		list_cards.removeClass("hide");
-	}else if(summaryIndex == 1){
-		list_cards.addClass("hide");
-		$(".list_cards li[data-reservation-type=requesting]").removeClass("hide");
-		$(".list_cards li[data-reservation-type=due]").removeClass("hide");
-	}else if(summaryIndex == 2){
-		list_cards.addClass("hide");
-		$(".list_cards li[data-reservation-type=used]").removeClass("hide");
-	}else if(summaryIndex == 3){
-		list_cards.addClass("hide");
-		$(".list_cards li[data-reservation-type=refund_cancel]").removeClass("hide");
+	function init(){
+		title = '';
+		time = '';
+		bindEvents();
 	}
-});
+	
+	function bindEvents(){
+		$(".card").on('click', '.booking_cancel', cancelReservation);///card item
+		popup_wrapper.on('click', '.popup_btn_close', closePopup);
+		popup_wrapper.on('click', '.btn_gray', closePopup);
+		popup_wrapper.on('click', '.btn_green', ajaxCancel);
+	}
+	
+	function cancelReservation(){
+		reservationId = $(this).attr("data-reservation-id");
+		title = $(this).parent().find(".tit").html();
+		time = $(this).parent().find(".item_reservation_date").text();
+		popup_wrapper.find(".pop_tit span").text(title);
+		popup_wrapper.find(".pop_tit .sm").text(time);
+		popup_wrapper.css("display", "block");
+		popup_wrapper.data("reservation-id", reservationId);
+	}
+	
+	function ajaxCancel(){
+		console.log("cancel!!!!" + reservationId);
+//		$.ajax({
+//			url : "/api/reservation/"+reservationId,
+//			method : "delete",
+//		});
+	}
+	
+	function closePopup(){
+		popup_wrapper.css("display", "none");
+	}
+	
+	return {
+		init: init
+	}
+})();
+
+var MyReservation = (function(){
+	var summaryItem = $(".my_summary");
+	var summaryIndex;
+	var reservationType;
+	
+	function init(){
+		summaryIndex = 0;
+		reservationType = 'all';
+		bindEvents();
+	}
+	
+	function bindEvents(){
+		summaryItem.on('click', 'li', summary);
+	}
+	
+	function summary(){
+		summaryItem.find("li").eq(summaryIndex).find("a").removeClass("on");
+		summaryIndex = $(this).index();
+		$(this).find("a").addClass("on");
+	}
+	
+	function showListCard(reservationType){
+		if(reservationType == 'all')
+			$(".list_cards li").removeClass("hide");
+		else{
+			$(".list_cards li").addClass("hide");
+			$(".list_cards li[data-reservation-type="+reservationType+"]").removeClass("hide");
+		}
+	}
+	
+	return {
+		init: init
+	}
+})();
 
 
-var reservationId;
-var title = '';
-var time = '';
-$(".booking_cancel").on("click", function(){
-	reservationId = $(this).attr("data-reservation-id");
-	title = $(this).parent().find(".tit").html();
-	time = $(this).parent().find(".item_reservation_date").text();
-	$(".popup_booking_wrapper").find(".pop_tit span").text(title);
-	$(".popup_booking_wrapper").find(".pop_tit .sm").text(time);
-	$(".popup_booking_wrapper").css("display", "block");
-	$(".popup_booking_wrapper").data("reservation-id", reservationId);
-});
 
 
-$(".popup_booking_wrapper .btn_green").on("click", function(){
-	$.ajax({
-		url : "/api/reservation/"+reservationId,
-		method : "delete",
-	});
-});
-
-
-$(".popup_btn_close").on("click", closePopup);
-$(".btn_gray").on("click", closePopup);
-
-function closePopup(){
-	$(".popup_booking_wrapper").css("display", "none");
-}
+//var reservationId;
+//var title = '';
+//var time = '';
+//$(".booking_cancel").on("click", function(){
+//	reservationId = $(this).attr("data-reservation-id");
+//	title = $(this).parent().find(".tit").html();
+//	time = $(this).parent().find(".item_reservation_date").text();
+//	$(".popup_booking_wrapper").find(".pop_tit span").text(title);
+//	$(".popup_booking_wrapper").find(".pop_tit .sm").text(time);
+//	$(".popup_booking_wrapper").css("display", "block");
+//	$(".popup_booking_wrapper").data("reservation-id", reservationId);
+//});
+//
+//$(".popup_booking_wrapper .btn_green").on("click", function(){
+//	$.ajax({
+//		url : "/api/reservation/"+reservationId,
+//		method : "delete",
+//	});
+//});

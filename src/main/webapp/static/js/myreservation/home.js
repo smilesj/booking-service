b485118183542b7e7c60f8dcb62f4a8d0c238400
smilesj <19smilesj@gmail.com>
2018-01-22 16:15:24 +0900

    my예약 타입별 보기

--- src/main/webapp/static/js/myreservation/home.js
+++ src/main/webapp/static/js/myreservation/home.js
@@ -5,7 +5,7 @@ $(function () {
 });
 
 var Popup = (function(){
-	var popup_wrapper = $(".popup_booking_wrapper");
+	var popup_wrapper = $('.popup_booking_wrapper');
 	var reservationId;
 	var title;
 	var time;
@@ -17,32 +17,32 @@ var Popup = (function(){
 	}
 	
 	function bindEvents(){
-		$(".card").on('click', '.booking_cancel', cancelReservation);///card item
+		$('.card').on('click', '.booking_cancel', cancelReservation);///card item
 		popup_wrapper.on('click', '.popup_btn_close', closePopup);
 		popup_wrapper.on('click', '.btn_gray', closePopup);
 		popup_wrapper.on('click', '.btn_green', ajaxCancel);
 	}
 	
 	function cancelReservation(){
-		reservationId = $(this).attr("data-reservation-id");
-		title = $(this).parent().find(".tit").html();
-		time = $(this).parent().find(".item_reservation_date").text();
-		popup_wrapper.find(".pop_tit span").text(title);
-		popup_wrapper.find(".pop_tit .sm").text(time);
-		popup_wrapper.css("display", "block");
-		popup_wrapper.data("reservation-id", reservationId);
+		reservationId = $(this).attr('data-reservation-id');
+		title = $(this).parent().find('.tit').html();
+		time = $(this).parent().find('.item_reservation_date').text();
+		popup_wrapper.find('.pop_tit span').text(title);
+		popup_wrapper.find('.pop_tit .sm').text(time);
+		popup_wrapper.css('display', 'block');
+		popup_wrapper.data('reservation-id', reservationId);
 	}
 	
 	function ajaxCancel(){
-		console.log("cancel!!!!" + reservationId);
+		console.log('cancel!!!!' + reservationId);
 //		$.ajax({
-//			url : "/api/reservation/"+reservationId,
-//			method : "delete",
+//			url : '/api/reservation/'+reservationId,
+//			method : 'delete',
 //		});
 	}
 	
 	function closePopup(){
-		popup_wrapper.css("display", "none");
+		popup_wrapper.css('display', 'none');
 	}
 	
 	return {
@@ -51,9 +51,10 @@ var Popup = (function(){
 })();
 
 var MyReservation = (function(){
-	var summaryItem = $(".my_summary");
+	var summaryItem = $('.my_summary');
 	var summaryIndex;
 	var reservationType;
+	var listCard = $('.list_cards li');
 	
 	function init(){
 		summaryIndex = 0;
@@ -66,17 +67,35 @@ var MyReservation = (function(){
 	}
 	
 	function summary(){
-		summaryItem.find("li").eq(summaryIndex).find("a").removeClass("on");
+		summaryItem.find('li').eq(summaryIndex).find('a').removeClass('on');
 		summaryIndex = $(this).index();
-		$(this).find("a").addClass("on");
+		$(this).find('a').addClass('on');
+		switch(summaryIndex){
+		case 0 : 
+			reservationType = 'all';
+			break;
+		case 1 : 
+			reservationType = ['requesting', 'due'];
+			break;
+		case 2 : 
+			reservationType = ['used'];
+			break;
+		case 3 : 
+			reservationType = ['refund_cancel'];
+			break;
+		}
+		showListCard(reservationType);
 	}
 	
 	function showListCard(reservationType){
+		
 		if(reservationType == 'all')
-			$(".list_cards li").removeClass("hide");
+			listCard.removeClass('hide');
 		else{
-			$(".list_cards li").addClass("hide");
-			$(".list_cards li[data-reservation-type="+reservationType+"]").removeClass("hide");
+			listCard.addClass('hide');
+			$.each(reservationType, function(index, type){
+				$('.list_cards li[data-reservation-type="'+type+'"]').removeClass('hide');
+			});
 		}
 	}
 	
@@ -85,25 +104,3 @@ var MyReservation = (function(){
 	}
 })();
 
-
-
-
-//var reservationId;
-//var title = '';
-//var time = '';
-//$(".booking_cancel").on("click", function(){
-//	reservationId = $(this).attr("data-reservation-id");
-//	title = $(this).parent().find(".tit").html();
-//	time = $(this).parent().find(".item_reservation_date").text();
-//	$(".popup_booking_wrapper").find(".pop_tit span").text(title);
-//	$(".popup_booking_wrapper").find(".pop_tit .sm").text(time);
-//	$(".popup_booking_wrapper").css("display", "block");
-//	$(".popup_booking_wrapper").data("reservation-id", reservationId);
-//});
-//
-//$(".popup_booking_wrapper .btn_green").on("click", function(){
-//	$.ajax({
-//		url : "/api/reservation/"+reservationId,
-//		method : "delete",
-//	});
-//});

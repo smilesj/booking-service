7b9775b8fcb84cd9bb261e8e0c50be13e482f9e7
smilesj <19smilesj@gmail.com>
2018-01-26 17:58:05 +0900

    ticket js 기능 작성 중

--- src/main/webapp/static/js/reserve/home.js
+++ src/main/webapp/static/js/reserve/home.js
@@ -1,4 +1,34 @@
 $(function () {
 	console.log("reserve!")
+    var tickets = []
+    $(".qty").each(function (i, e) {
+        tickets.push(new Ticket('#' + $(e).attr('id')));
+    });
 });
 
+class Ticket {
+
+	constructor(id){
+		this.root = id;
+		this.count = 0;
+		this.bindEvents();
+		console.log(id+ " ticket create!");
+	}
+	
+	function bindEvents(){
+		this.on("click", ".ico_minus3", this.minus.bind(this));
+		this.on("click", ".ico_plus3", this.plus.bind(this));
+	}
+	
+	function plus(){
+		this.count++;
+		$(this).find(".total_price").html(this.count);
+		console.log(this.count);
+	}
+	
+	function minus(){
+		this.count--;
+		$(this).find(".total_price").html(this.count);
+		console.log(this.count);
+	}
+}
